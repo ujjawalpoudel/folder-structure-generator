a0f9901632fc3281dda742706c9bfb8e8f01299d
Ujjawal Poudel <ujjawalpoudel@gmail.com>
2026-04-28 21:31:03 -0400

    Prepare release 0.0.3

--- folder_structure/__init__.py
+++ folder_structure/__init__.py
@@ -1,4 +1,4 @@
 from .folder_structure_generator import DEFAULT_IGNORED_FOLDERS, FolderStructureGenerator
 
 __all__ = ["DEFAULT_IGNORED_FOLDERS", "FolderStructureGenerator"]
-__version__ = "0.1.0"
+__version__ = "0.0.3"
